f8ae6adab63fa1bf3118aec065165ba74644dacb
Laisses <71999249+Laisses@users.noreply.github.com>
2021-04-11 11:16:33 -0300

    Separa o CSS e o JavaScript do TecFlix

---
+++ tecflix.js
@@ -0,0 +1,238 @@
+	var ficcaoCientifica = [
+	{
+		titulo: "Ex_Machina: Instinto Artificial",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMTUxNzc0OTIxMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/EoQuVnKhxaM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Anon",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjE0MjIwMDE2MV5BMl5BanBnXkFtZTgwMzM5MDQzNTM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/RJOoYhQcQBI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Tron: Uma Odisséia Eletrônica",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMzZhNjYyZDYtZmE4MC00M2RlLTlhOGItZDVkYTVlZTYxOWZlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/_ReyhCkn9Bg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "O Congresso Futurista",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjE5MTUzNDk3M15BMl5BanBnXkFtZTgwMTUwNTQ5MTE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Y0JD_ARhvaU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Black Mirror: Bundersnatch",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjM5MzgzMjM3OF5BMl5BanBnXkFtZTgwMzQ2MzQwNzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video:'<iframe width="560" height="315" src="https://www.youtube.com/embed/htfs3DQS9Jg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	}
+];
+
+var acao = [
+	{
+		titulo: "Blackhat",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMTg1NDUyMzk5NV5BMl5BanBnXkFtZTgwOTk1NzUxMzE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/jZ1ZDlLImF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Ghost in the Shell (2017)",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMzJiNTI3MjItMGJiMy00YzA1LTg2MTItZmE1ZmRhOWQ0NGY1XkEyXkFqcGdeQXVyOTk4MTM0NQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/G4VmJcZR0Yg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Jogos de Guerra",
+		imagem:  "https://m.media-amazon.com/images/M/MV5BMTMyMTE3OTk3NF5BMl5BanBnXkFtZTcwOTkwNDc3NA@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/hbqMuvnx5MU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Matrix",
+		imagem:  "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/2KnZac176Hs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "A Senha: Swordfish",
+		imagem: "https://m.media-amazon.com/images/M/MV5BNzk5ZmQxMWYtM2QyNi00MTY3LTlmNjItYjUwODY3Y2YwOTIwXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/rjumeQhhPmU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+];
+
+var biografia = [
+	{
+		titulo: "O Jogo da Imitação",
+		imagem: "https://m.media-amazon.com/images/M/MV5BOTgwMzFiMWYtZDhlNS00ODNkLWJiODAtZDVhNzgyNzJhYjQ4L2ltYWdlXkEyXkFqcGdeQXVyNzEzOTYxNTQ@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/nuPZUUED5uk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Snowden",
+		imagem:  "https://m.media-amazon.com/images/M/MV5BMTg2MzYzNzgzOF5BMl5BanBnXkFtZTgwOTg4NzQ4OTE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/QlSAiI3xMh4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "O Quinto Poder",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjY1MTY5NTg3M15BMl5BanBnXkFtZTgwMDQyMjgwMDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZT1wb8_tcYU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Steve Jobs",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjE0NTA2MTEwOV5BMl5BanBnXkFtZTgwNzg4NzU2NjE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/aEr6K1bwIVs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Rede Social",
+		imagem: "https://m.media-amazon.com/images/M/MV5BOGUyZDUxZjEtMmIzMC00MzlmLTg4MGItZWJmMzBhZjE0Mjc1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/lB95KLmpLR4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	}
+];
+
+var animeEDesenho = [
+	{
+		titulo: "Tron: A Resistência",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMmI5OWZiYzktMDc3OC00Y2Q5LWE3MzAtOTUyYTVlNzAzZGYyXkEyXkFqcGdeQXVyNjExODE1MDc@._V1_UY268_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/yb3-FsWhYGQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Psycho-Pass (série)",
+		imagem: "https://m.media-amazon.com/images/M/MV5BYmNlMTYzNDMtZjI5NS00NTdjLTlkYWMtMWMzZWJjODY2YzAzXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR3,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/YzuJnyebc40" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Battle Programmer Shirase",
+		imagem: "https://m.media-amazon.com/images/M/MV5BNmZlOWJjZmItNWM5ZC00MDM5LTg5ZGQtMTdkZjAxODk2OTY4XkEyXkFqcGdeQXVyMTA1OTEwNjE@._V1_UY268_CR5,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/6WxJECOFg8w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	}, 
+	{
+		titulo: "Ghost in the Shell (1995)",
+		imagem: "https://m.media-amazon.com/images/M/MV5BYWRiYjQyOGItNzQ1Mi00MGI1LWE3NjItNTg1ZDQwNjUwNDM2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/SvBVDibOrgs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Guerras de Verão",
+		imagem: "https://m.media-amazon.com/images/M/MV5BNDUzMzVmZGEtNjE4Ny00NmZkLThiMWEtNjJjY2Y3MzlkYzg1XkEyXkFqcGdeQXVyNjYyMTYxMzk@._V1_UY268_CR3,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/HjLE8BmWfKA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	}
+];
+
+var comedia = [
+	{
+		titulo: "Mythic Quest: Raven's Banquet",
+		imagem: "https://m.media-amazon.com/images/M/MV5BYjUyMmJjZmUtY2VhZi00YTY0LTg5NmMtYzU3NDM4N2YzNzk3XkEyXkFqcGdeQXVyNDk3ODk4OQ@@._V1_UY268_CR1,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/6SsqG4uBVPs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Os Estagiários",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjM1MzczMDgwOV5BMl5BanBnXkFtZTcwMDM4NjM2OQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/iJNvxWemhI4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Silicon Valley (série)",
+		imagem: "https://m.media-amazon.com/images/M/MV5BOTcwNzU2MGEtMzUzNC00MzMwLWJhZGItNDY3NDllYjU5YzAyXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/lqgM8ZdAqKo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "The IT Crowd (série)",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjE5MThjMzAtNWVmNC00YThkLTlmNzktMTM3Yzk4YTZhMTgwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR2,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/MwwTfkXk7U0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Free Guy: Assumindo Controle",
+		imagem: "https://m.media-amazon.com/images/M/MV5BYTNjNzFiZTItNDM5NS00ODBhLTk5NmMtYzUyZWRmOWJjOTI3XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/CMynkj1qAYo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	}
+];
+
+var drama = [
+	{
+		titulo: "Ela",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjA1Nzk0OTM2OF5BMl5BanBnXkFtZTgwNjU2NjEwMDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/6QRvTv_tpw0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Invasores: Nenhum Sistema está A Salvo",
+		imagem: "https://m.media-amazon.com/images/M/MV5BYmRiYjQ0OGQtYTAzMi00OGVjLWE4YTQtM2Q4YjBlZTBhMWM5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UY268_CR4,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZmRTPmHUFr4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "A Rede",
+		imagem: "https://m.media-amazon.com/images/M/MV5BZTg5YmM5NzktN2Q0OS00YzE1LThkOTktNTE1NTJkZmExNmIxXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/4JqyRXXwojo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Mr. Robot (série)",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMzgxMmQxZjQtNDdmMC00MjRlLTk1MDEtZDcwNTdmOTg0YzA2XkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/8qZYW_1hj2g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Piratas da Informática",
+		imagem: "https://m.media-amazon.com/images/M/MV5BNDc2NTE0NzE4N15BMl5BanBnXkFtZTgwMDQ5MzgwMzE@._V1_UY268_CR3,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/qma8YVHev0c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	}
+];
+
+var documentario = [
+	{
+		titulo: "Revolution OS",
+		imagem:  "https://m.media-amazon.com/images/M/MV5BMTgyNDU0NTIwNV5BMl5BanBnXkFtZTcwNzE4MDQyMQ@@._V1_UY268_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/NrI-0u4npGo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "TPB AFK",
+		imagem: "https://m.media-amazon.com/images/M/MV5BMjU4MzY4ODg0MV5BMl5BanBnXkFtZTcwNTE2MjY5OA@@._V1_UY268_CR4,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/G6qcJt2NPY8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Atari: Game Over",
+		imagem: "https://m.media-amazon.com/images/M/MV5BZWNiNTIwNmItMWViMC00ZmE5LTk4YmQtNTlmMTQwYTg4ZWI2XkEyXkFqcGdeQXVyMTI0Nzk5NTQ2._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/4wjMn6yjOjQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "Codegirl",
+		imagem: "https://m.media-amazon.com/images/M/MV5BNzQyMzc5NzU2NF5BMl5BanBnXkFtZTgwODg0MzQxNzE@._V1_UY268_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/cRb5iel-3Ck" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	},
+	{
+		titulo: "O Código Bill Gates",
+		imagem: "https://m.media-amazon.com/images/M/MV5BNjI4OWJlOGUtMjhlNy00MTA3LTkzZTgtMTZiZjU5YWJjZWZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/yOCjrGevsdo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	}
+];
+
+var todosFilmes = ficcaoCientifica
+	.concat(acao)
+	.concat(biografia)
+	.concat(animeEDesenho)
+	.concat(comedia)
+	.concat(drama)
+	.concat(documentario);
+
+		function mostraTrailer(i) {
+			document.querySelector("#trailer-popup").innerHTML = filmesAtuais[i].video;
+			document.querySelector("#trailer-container").classList.remove("esconder");
+		}
+
+		function escondeTrailer() {
+			document.querySelector("#trailer-container").classList.add("esconder");
+			document.querySelector("#trailer-popup").innerHTML = "";
+		}
+
+		var filmesAtuais;
+
+function mostraFilmes(nome, filmes) {
+	// ${variavel}
+	var filmesHTML = "";
+			filmesAtuais = filmes;
+
+	for (var i = 0; i < filmes.length; i++) {
+		filmesHTML += `
+			<div class="filme" onclick="mostraTrailer(${i})">
+			  	<img src="${filmes[i].imagem}" />
+					<span class="titulo">
+						${filmes[i].titulo}
+					</span>
+			</div>
+		`;
+	}
+	
+	document.querySelector("#generoFilme").innerHTML = nome;
+	document.querySelector("#listaFilmes").innerHTML = filmesHTML;
+}
+
+document.querySelector("#btnFechaTrailer").onclick = escondeTrailer;
+mostraFilmes("Todos os gêneros", todosFilmes);
